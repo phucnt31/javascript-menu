007cb5b446ace81b72a6859cc138dffce910e7a7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-22 18:45:21 +0700

    Filter function finished

--- app.js
+++ app.js
@@ -74,7 +74,23 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
+// filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) return menuItem;
+    });
+    if (category === "all") displayMenuItems(menu);
+    else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+// load items
 window.addEventListener("DOMContentLoaded", function (e) {
   displayMenuItems(menu);
 });
